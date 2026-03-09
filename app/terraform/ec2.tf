5875a74938a43d18704044db88eb49c5e86e996e
@kki... <akashbajgire12@gmail.com>
2026-03-09 11:56:54 +0530

    Update ec2.tf

--- app/terraform/ec2.tf
+++ app/terraform/ec2.tf
@@ -1,6 +1,9 @@
-resource "aws_instance" "devops_server" {
+resource "aws_instance" "web_server" {
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
+
+  subnet_id              = subnet-0540b1bb805beecbb
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "DevOps-Server"
